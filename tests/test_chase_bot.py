70cad8b99169b016dd253976f1bdde52c4cac4af
Greg Olmschenk <golmschenk@gmail.com>
2015-02-07 11:26:11 -0500

    Refactor chase_bot tests to use a fixture for chase_bot

--- tests/test_chase_bot.py
+++ tests/test_chase_bot.py
@@ -8,19 +8,22 @@ import pytest
 class TestSimpleChaseConnection:
     """Tests the ChaseBot class."""
 
-    def test_bot_can_login(self):
-        """Tests that a manually activated bot can get to the chase transaction page."""
-        chase_bot = ChaseBot()
+    @pytest.fixture
+    def chase_bot(self):
+        """
+        Creates a ChaseBot.
+        """
+        return ChaseBot()
 
+    def test_bot_can_login(self, chase_bot):
+        """Tests that a manually activated bot can get to the chase transaction page."""
         chase_bot.login()
 
         assert "Chase Online - My Accounts" in chase_bot.browser.title
         assert "https://chaseonline.chase.com/MyAccounts.aspx" in chase_bot.browser.current_url
 
-    def test_bot_can_get_to_chase_transaction_page(self):
+    def test_bot_can_get_to_chase_transaction_page(self, chase_bot):
         """Tests that a manually activated bot can get to the chase transaction page."""
-        chase_bot = ChaseBot()
-
         chase_bot.go_to_transaction_page()
 
         assert "Account Activity" in chase_bot.browser.title
