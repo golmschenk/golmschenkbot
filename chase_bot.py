a04152c4ada420176bc4535c4937b3a28923312e
Greg Olmschenk <golmschenk@gmail.com>
2015-02-06 22:44:50 -0500

    ChaseBot with login

---
+++ chase_bot.py
@@ -0,0 +1,23 @@
+"""
+Module for bot to Chase interaction.
+"""
+
+from selenium import webdriver
+from secret import Secret
+
+class ChaseBot():
+
+    def __init__(self):
+        self.browser = webdriver.Chrome()
+
+    def login(self):
+        """
+        Goes to the transactions page.
+        :return:
+        """
+        self.browser.get("https://www.chase.com")
+        username_input = self.browser.find_element_by_id("usr_name_home")
+        username_input.send_keys(Secret.chase_username)
+        password_input = self.browser.find_element_by_id("usr_password_home")
+        password_input.send_keys(Secret.chase_password)
+        self.browser.find_element_by_xpath("//a[@data-pt-name='unknwnlogin']").click()
---
+++ tests/test_chase_bot.py
@@ -0,0 +1,27 @@
+"""
+Tests the chase_bot module.
+"""
+
+from ..chase_bot import ChaseBot
+import pytest
+
+class TestSimpleChaseConnection():
+    """Tests the ChaseBot class."""
+
+    def test_bot_can_login(self):
+        """Tests that a manually activated bot can get to the chase transaction page."""
+        chase_bot = ChaseBot()
+
+        chase_bot.login()
+
+        assert "Chase Online - My Accounts" in chase_bot.browser.title
+        assert "https://chaseonline.chase.com/MyAccounts.aspx" in chase_bot.browser.current_url
+
+    def test_bot_can_get_to_chase_transaction_page(self):
+        """Tests that a manually activated bot can get to the chase transaction page."""
+        chase_bot = ChaseBot()
+
+        chase_bot.go_to_transaction_page()
+
+        assert "Account Activity" in chase_bot.browser.title
+        assert "https://chaseonline.chase.com/MyAccounts.aspx" in chase_bot.browser.current_url
