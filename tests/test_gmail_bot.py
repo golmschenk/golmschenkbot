f8f5965eb2138332f412e4bc4c7692a188148b49
Greg Olmschenk <golmschenk@gmail.com>
2015-02-13 18:58:03 -0500

    Test for extracting the code

--- tests/test_gmail_bot.py
+++ tests/test_gmail_bot.py
@@ -35,7 +35,7 @@ class TestGmailBot:
         gmail_bot.login()
 
         assert "Gmail" in gmail_bot.browser.title
-        assert "https://mail.google.com/mail/#inbox" in gmail_bot.browser.current_url
+        assert "https://mail.google.com/mail/" in gmail_bot.browser.current_url
 
     def test_can_open_first_email(self, gmail_bot):
         """
@@ -48,3 +48,16 @@ class TestGmailBot:
         gmail_bot.open_first_email()
 
         assert "Your Requested Online Banking" in gmail_bot.browser.title
+
+    def test_can_find_code(self, gmail_bot):
+        """
+        Test that the bot can reach the inbox.
+        :param gmail_bot:
+        :return:
+        """
+        gmail_bot.login()
+        gmail_bot.open_first_email()
+
+        code = gmail_bot.extract_code()
+
+        assert code.isdigit()
